a1e1dc99c6c83ab59fb5cf5c33ed658c44b84d04
JoranManoel <joran.manoel@hotmail.com>
2024-08-17 23:44:37 -0300

    Versão estável do sorteio com JS

---
+++ js/script.js
@@ -0,0 +1,101 @@
+    
+// Jovens
+// var nomes = ["167;Vitória Raquel;;Vera Lúcia ", "543;Daniel Henrique de Souza Batista;;Sublime 1", "470;Karen Gabriele;;Sublime 1", "135;David Mateus;;Veneza 2", "345;Lorruama Gomes;;Veneza 1", "232;Ester Vitória;;Coquinho 2", "261;Savio Elias;;Jardim Cajueiro 2", "262;Layla Monique;;Jardim Cajueiro 2", "496;Thayna vitoria;;Jardim Cajueiro 2", "269;Sara Rebeca;;Jardim Cajueiro 2", "270;Adrielly Cayllane;;Jardim Cajueiro 2", "271;Thays Vitoria;;Jardim Cajueiro 2", "584;Jan Sabino;;Coquinho 1", "620;Mariana Ribeiro;;Coquinho 1", "429;Lucas Carlos;;Sublime 1", "346;Micaele Vitória;;Veneza 1", "594;Evellyn oliveira;;Veneza 2", "426;Anna Clara;;Sublime 1", "133;Irene Bernardi;;Veneza 2", "80;José Samuel;;Veneza 1", "324;Jeremias Jorge;;Poço de Jacó", "587;Paulo Henrique;;Veneza 2", "136;Elisa Bezerra;;Veneza 2", "223;Júlia Sophya;;Coquinho 2", "615;Miqueias Moises;;Veneza 1", "604;Vitor Manuel;;Sublime 1", "547;Ryan Alexandre S da silva;;Veneza 2", "592;Yurika Dionízio;;Veneza 2", "0;Ana Lídia Martins ;;Veneza 1", "0;Samuel Martins ;;Veneza 1", "425;Lidson Valmir;;Sublime 1", "424;Lindssy vitória;;Sublime 1"];
+
+
+// Igreja
+var nomes = ["87;Paulo Eduardo;Anne Karoline;Veneza 1", "494;Adenami;Rosangela;Jardim Cajueiro 2", "147;Joséfa Rosa;;Veneza 2", "47;Edvan Camilo;;Veneza 1", "452;Saulo;Priscila;Jardim Cajueiro 1", "387;Darciele Souza;Linaldo jose;Sublime 1", "256;José Francisco;Dalete da Silva;Jardim Cajueiro 2", "0;Jacilene Maria;;Visitante", "27;Diogo;Jailda;Coquinho 1", "0;Deividy Jose;Natali Maria;Visitante", "602;Henrique Leonardo;Cleidiane severina;Sublime 1", "372;Pb Euclides;Mônica;Poço de Jacó", "69;Elizan;Marileide;Veneza 1", "206;José Roberto;Adriana Fernandes;Coquinho 2", "204;Dc Elias;Ana;Coquinho 2", "332;Luciana Souza;;Veneza 1", "246;Severina Diogo;;Vera Lúcia", "117;Valdir;Lucineide;Veneza 2", "5;Jeilson;Maria;Coquinho 1", "293;Israel Jaco;Jacilene;Jardim Cajueiro 2", "252;Cleber;Iraneide;Jardim Cajueiro 2", "230;Ana Santos;;Coquinho 2", "126;Bina Barbosa;;Veneza 2", "16;Gracilene Josefa;;Coquinho 1", 
+"497;Helio luiz;Sonia da silva;Jardim Cajueiro 2", "562;Dc José Ricardo;Gilca;Sublime 2", "3;Dc Felipe Godim; Dayane Godim;Vera Lúcia", "395;Jose Carlos;Edjane margarida;Sublime 1", "329;Jailson;Denise;Poço de Jacó", "334;Rosemary Silva;;Veneza 1", "359;Moisés;Patrícia;Veneza 1", "102;Leandro;Adeilda;Jardim Cajueiro 1", "388;Marcos Alberto;Débora silva;Sublime 1", "348;Andreza Cristina ;;Veneza 1", "257;Dc Paulo Gabriel;Marilene;Jardim Cajueiro 2", "609;Agnaldo;Josilene;Veneza 1", "58;José Leonardo;Vanderleia;Veneza 1", "289;José Geraldo;Maria do Socorro;Jardim Cajueiro 2", "122;Maria Eunice;;Veneza 2", "534;Rogério;Jadilene;Coquinho 1", "619;Moises Almeida;Jeane Almeida;Veneza 1", "322;Dc Jorge;Edilene;Poço de Jacó", "512;Wellington;Edilene;Veneza 1", "100;Samuel Gabriel;Cilira Diacui;Veneza 2", "364;Vicente;Elivânia;Veneza 1 ", "146;Elias Antônio;Shirlene Teixeira;Veneza 2", 
+"518;Maria do Socorro;;Veneza 2", "0;Marinaldo;Sulamita Risia;Visitante", "282;Maria de Deus;;Jardim Cajueiro 2", "290;Sebastiao Barbosa;Miriam Martins;Jardim Cajueiro 2", "300;Maria Luiza;;Vera Lúcia", "529;Ezequiel;Hosana;Veneza 2", "164;Samuel bezerra;Carla do Carmo;Veneza 2", "114;Graciênia Costa;;Veneza 2", "113;Luciana costa;;Veneza 2", "0;Klebison ;Sueli Cristina ;Visitante", "437;José Antônio;Adilma;Sublime 1", "63;Cosmo;Ana Claudia;Veneza 1", "209;Vera Lúcia da Silva;;Coquinho 2", "575;Sandro;Luciana;Coquinho 2", "131;Josefa Nascimento;;Veneza 2", "149;André Marcolino;Andreia Mônica;Veneza 2", "205;Pedro Sebastião;Luci Ferreira;Coquinho 2", "320;Jair;Jemima;Poço de Jacó", "500;Mariano;Juliana;Veneza 1", "532;Evertom;Adriana;Jardim Cajueiro 2", "412;Ana Paula;;Sublime 1", "515;Everaldo;Maria josé;Veneza 2", "381;Maria Edinete;;Veneza 1", "441;Wanderson;Daniela;Sublime 1", 
+"9;Edilene Eunice;;Coquinho 1", "413;Maria da saúde;;Sublime 1", "251;Silvio;Ezilma;Jardim Cajueiro 2", "104;José Aparecido;Amara ferreira;Veneza 2", "394;Adalberto;Jacilene;Sublime 1", "359;Moisés;Patrícia;Veneza 1", "0;Benidito;Osana;Visitante", "611;David Jessimo;Joyce Rodrigues;Veneza 1", "178;José Alexandre;Natacha Cristina;Vera Lúcia", "96;Luciano Manuel;Andreia María;Jardim Cajueiro 1", "24;Marlene Maria;;Coquinho 1", "13;Marli Tavares;;Coquinho 1", "411;Josafá;Solange;Sublime 1", "439;Valmir;Leidiane;Sublime 1", "141;Carmen Lucia;;Veneza 2", "51;Cristiano;Vanda;Veneza 1", "600;Josué cordeiro;Mercia Maria;Sublime 1", "8;Eliane Lira;;Coquinho 1", "48;Maria Sueli;;Veneza 1", "116;Suzana Maria;;Veneza 2", "579;Cosmo Germano;Josefa Germana;Veneza 2", "97;Fábio Jr.;Thaisa Santos ;Veneza 2", "143;Dc José Romero;Edneide Josefa;Veneza 2", "78;Severino Bernardo;Maria Helena;Veneza 1", 
+"527;Eduardo;Rosa;Vera Lúcia", "33;Lucio;Luciana;Veneza 1", "287;Porfirio;Virginia;Jardim Cajueiro 2", "280;Marco;Rosilda;Jardim Cajueiro 2", "365;Alexandro Felix ;Ana Jéssica;Veneza 1 ", "614;Maria Cleonice;;Veneza 1", "67;Rivaldo;Ana Cristina;Veneza 1", "288;Bete maria;;Jardim Cajueiro 2", "39;Lindoarte;Amanda;Veneza 1", "83;Inaldo;Cacilda;Veneza 1", "380;Gertânia;Everaldo;Veneza 1", "52;Eva Maria;;Veneza 1", "53;José Damásio;Wellida;Veneza 1", "276;Benedita Cecilia;;Jardim Cajueiro 2", "43;Jocilio;Rosineide;Veneza 1", "32;Dc Jefferson;Ana Paula;Veneza 1", "41;Felipe;Thays;Veneza 1", "45;João;Erivalda;Veneza 1", "35;Douglas;Elaine;Veneza 1", "31;Pb Arão;Aurizete;Veneza 1", "54;Abiezer;Adriana;Veneza 1", "121;Djane Alves ;;Veneza 2", "535;Helder;Luciene;Coquinho 1", "357;Cosmo Ferreira;;Veneza 1", "612;Lucas Almeida;Dasilvania Almeida;Veneza 1", "132;Elias Julios;;Jardim  Cajueiro 1"];
+
+
+//CÓDIGO DE SORTEIO
+var esperar = false; //Aguarda o sorteio para quando apertar TAB ele não fique chamando a função
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Tab") {
+        if(esperar == false){
+            sortear();
+        }
+    }
+  });
+
+function sortear() {
+    var nome = document.getElementById("nome");
+    var congregacao = document.getElementById("congregacao");
+    var loader = document.getElementById("loader");
+    var nomesChamadosList = document.getElementById("nomesChamados");
+
+    esperar = true;
+
+    if (nomes.length === 0) {
+        esperar = false;
+        nome.textContent = "FIM! 😢";
+        congregacao.innerHTML = '';
+        return;
+    }
+
+    //Faz o sorteio pelo indice da lista
+    var indice = Math.floor(Math.random() * nomes.length);
+    var nomeSorteado = nomes[indice];
+    nomes.splice(indice, 1); // Remove o elemento sorteado do array
+
+    //Implementar o tratamento de String da variável nomeSorteado para pegar o nome, nome do conjuge e congregação
+    var splitNome = nomeSorteado.split(';');
+    var congregacaoDoNomeSorteado = splitNome[3];
+    if(splitNome[2]!=""){
+        nomeApresentado = splitNome[1]+'<br><b style="color:var(--cor-primaria);">&</b><br>'+splitNome[2];
+    }else{
+        nomeApresentado = splitNome[1];
+    }
+
+    // Cria o loader----------------------------|
+    var loading = document.createElement('div');
+    loading.classList.add('custom-loader');
+    nome.innerHTML = '';// Limpa a tela
+    congregacao.innerHTML = 'Sorteando...';
+    loader.append(loading);
+
+    // Código que será executado após 3 segundos
+    setTimeout(function() {
+        loader.innerHTML = ''; // Limpa a tela do sorteio
+        nome.innerHTML = nomeApresentado; // Adiciona o nome sortiado para ser mostrado na tela
+        congregacao.innerHTML = "<b style='color:var(--cor-secundaria);'>" + congregacaoDoNomeSorteado + "</b>"; // Adiciona a congregação do nome sortiado para ser mostrado na tela
+
+        // Adiciona o nome na lista de nomes chamados
+        var novoLi = document.createElement("li");
+        novoLi.className = "pessoa";
+        function removeBRFromString(inputString) {
+            // Verifica se a string contém o elemento '<br>'
+            if (inputString.includes('<br>')) {
+              // Utiliza o método 'replace' para remover o elemento '<br>'
+              // O primeiro argumento é o padrão a ser substituído (no caso, '<br>')
+              // O segundo argumento é o valor de substituição, neste caso, uma string vazia para removê-lo
+              inputString = inputString.replace(/<br>/gi, ' ');
+            }
+          
+            return inputString;
+          }
+        var nomeFormatado = removeBRFromString(nomeApresentado);
+
+        // Adiciona classe para os itens existentes e define atraso de animação
+        var items = document.querySelectorAll(".pessoa");
+        items.forEach(function(item, index) {
+            item.style.transitionDelay = (0.9) + "s";
+            item.classList.add("shift-down");
+            nomesChamadosList.classList.add("shift-down");
+        });
+        novoLi.classList.add("fade-in");//adiciona classe de animação 
+
+        //Adiciona na ordem do ultimo nome sorteado no topo da lista 
+        nomesChamadosList.insertBefore(novoLi, nomesChamadosList.firstChild); 
+
+        novoLi.innerHTML = "<h3>" + nomeFormatado + "</h3><b style='color:white;'>" + congregacaoDoNomeSorteado + "</b>";
+
+        esperar = false;
+        
+    },1000);
+}
+
