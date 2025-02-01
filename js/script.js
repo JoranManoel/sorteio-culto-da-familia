4fc36c8e1fe93bede5b5eeef8b3ca894e266d214
JoranManoel <joran.manoel@hotmail.com>
2025-02-01 13:28:11 -0300

    Alteração no JS para buscar lista automatico

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
-// JOVENS
-// var nomes = ["605;Evellyn Vitória;;Veneza 1", "0;Victor Manoel ;;Sublime 1", "0;Ketlin Fernanda ;;Vera Lúcia", "347;Betessemes Beatriz ;;Veneza 1", "0;Samuel Martins ;;Veneza 1", "0;Ana lidia;;Veneza 1", "625;João Lucas;;Veneza 1", "202;Thawan Renato;;Vera Lúcia", "223;Júlia Sophya;;Coquinho 2", "220;Iasmim Mota;;Coquinho 2", "214;Arthur Gustavo;;Coquinho 2", "222;Nicolas Leandro;;Coquinho 2", "235;Radamés Reuel;;Vera Lúcia", "268;Caio Israel;;Jardim Cajueiro 2", "190;Amanda Lacerda;;Vera Lúcia", "542;Tais de Souza Batista;;Sublime 1", "543;Daniel Henrique de Souza Batista;;Sublime 1", "327;Yasmim Ruana;;Poço de Jacó", "127;Ana Beatriz;;Veneza 2", "594;Evellyn oliveira;;Veneza 2", "152;Emily Eduarda;;Veneza 2", "622;Adna Caroline;;Veneza 2", "574;Guilherme Lima Soares;;Coquinho 2", "240;Ana Clara;;Vera Lúcia", "247;Israelen Vitória;;Vera Lúcia", "189;Alessandro Rufino;;Vera Lúcia", "192;Sara Ferreira;;Vera Lúcia", "47;Edvan Camilo;;Veneza 1", "137;Thiago Paulo;;Veneza 2", "232;Ester Vitória;;Coquinho 2", "236;Fellipe Gabriel;;Vera Lúcia", "636;Júlia Beatriz;;Veneza 2", "587;Paulo Henrique;;Veneza 2", "167;Vitória Raquel;;Vera Lúcia ", "345;Lorruama Gomes;;Veneza 1", "423;Paulo Henrique;;Sublime 1", "547;Ryan Alexandre S da silva;;Veneza 2", "493;Sellyne Erlana;;Jardim Cajueiro 2", "0;sonia valentin;;Sublime 1", "429;Lucas Carlos;;Sublime 1", "185;Kamylly Mayara;;Vera Lúcia", "196;Áquila Beniel;;Vera Lúcia", "427;Evelyn Julia;;Sublime 1", "620;Mariana Ribeiro;;Coquinho 1", "491;Oziel Martins;;Veneza 2", "509;Gisele Helen;;Poço de Jacó", "316;Crislayne Eduarda;;Poço de Jacó", "107;Millayne Sales;;Jardim Cajueiro 1"];
+async function carregarPresencas() {
+    try {
+        let response = await fetch("127.0.0.1:80/a33/listar.php");
+        let data = await response.json();
+        
+        if (data.success) {
+            let nomes = data.presencas.map(p => `${p.id_casal};${p.nome};${p.nome_conjuge};${p.congregacao}`);
+            
+            // Verifica se há casais para o sorteio
+            if (nomes.length === 0) {
+                alert("Nenhuma presença registrada hoje! Cadastre as presenças antes do sorteio.");
+            } else {
+                console.log("Casais para o sorteio:", nomes);
+                // Aqui você pode adicionar o código para iniciar o sorteio
+            }
+        } else {
+            console.error("Erro ao carregar presenças:", data.message);
+        }
+    } catch (error) {
+        console.error("Erro ao buscar presenças:", error);
+    }
+}
 
-// IGREJA
-var nomes =["227;Jaqueline Lopes;;Coquinho 2", "230;Ana Santos;;Coquinho 2", "102;Leandro;Adeilda;Jardim Cajueiro 1", "296;Wellington;Jezania;Vera Lúcia", "358;Rosemary Alves;;Veneza 1", "69;Elizan;Marileide;Veneza 1", "573;Maria de Fátima Soares;;Coquinho 2", "208;Joelma Maria;;Coquinho 2", "369;Arlindo;Jaselma;Coquinho 2", "536;Ronildo;Ana;Coquinho 2", "227;Jaqueline Lopes;;Coquinho 2", "230;Ana Santos;;Coquinho 2", "0;Jocilio ;Rosineide ;Veneza 1", "176;Valdira de França;;Vera Lúcia", "175;Flávio;Valdenize;Vera Lúcia", "237;Janicleide Maria;;Vera Lúcia", "304;José Eugênio;Amara Valéria;Vera Lúcia", "635;Severino Ricardo;Flávia Cecília;Veneza 2", "518;Maria do Socorro;;Veneza 2", "591;Alex Francisco;Elisabete Lopes;Veneza 2", "529;Ezequiel;Hosana;Veneza 2", "572;Elias cosmo;Ivanise silva;Veneza 2", "579;Cosmo Germano;Josefa Germana;Veneza 2", "126;Bina Barbosa;;Veneza 2", "546;Miriam Santana Barbosa;;Veneza 2", "146;Elias Antônio;Shirlene Teixeira;Veneza 2", "144;Josué Santana;Maria Nazaré;Veneza 2", "131;Josefa Nascimento;;Veneza 2", "148;José Rômulo;Roseane Caroline;Veneza 2", "103;José Ramos;Josefa Maria;Veneza 2", "106;José Manuel;Marilene Edite;Veneza 2", "251;Silvio;Ezilma;Jardim Cajueiro 2", 
-"121;Djane Alves ;;Veneza 2", "145;Agnaldo;Clécia;Veneza 2", "116;Suzana Maria;;Veneza 2", "108;Paulo José;Luana;Veneza 2", "515;Everaldo;Maria josé;Veneza 2", "97;Fábio Jr.;Thaisa Santos ;Veneza 2", "149;André Marcolino;Andreia Mônica;Veneza 2", "122;Maria Eunice;;Veneza 2", "34;Luiz;Girlene;Veneza 1", "33;Lucio;Luciana;Veneza 1", "439;Valmir;Leidiane;Sublime 1", "358;Rosemary Alves;;Veneza 1", "199;Claudenir;Josiane;Vera Lúcia", "116;Suzana Maria;;Veneza 2","368;Otoniel;Manuela;Poço de Jacó", "143;Dc José Romero;Edneide Josefa;Veneza 2", "0;Cristiane ;;Sublime 1", "144;Josué Santana;Maria Nazaré;Veneza 2", "0;Cassia Alves ;ronaldo;Sublime 1", "0;Severina marques ;;Sublime 1", "0;linaldo;darciele  ;Sublime 1", "0;Bernadete ;;Sublime 1", "0;Alessandra ;;Sublime 1", "0;Cintia ;ilanildo;Sublime 1", "0;Micaele ;Luciano ;Coquinho 1", "0;edvaldo;lucia;Sublime 1", "0;Diogo ;jailda ;Coquinho 1", "519;Allany Martins;;Veneza 2", "3;Dc Felipe Godim; Dayane Godim;Vera Lúcia", "0;Josué ;mercia;Sublime 1", "585;Carlos Alexandre;Maria Cibele;Vera Lúcia", "0;Gilson ;Andreia ;Sublime 1", "0;DC Jesse ;Erica ;Sublime 1", "0;helton;Mércia ;Sublime 1", "0;Tácio ;mikeline;Coquinho 1", "0;cintisllaine;Marcos ;Sublime 1", 
-"178;Rosemare Maria;;Vera Lúcia", "241;Pb Edvaldo;Erotides;Vera Lúcia", "221;Pb Antônio;Amara;Vera Lúcia", "300;Maria Luiza;;Vera Lúcia", "0;Rogério ;jadilene;Coquinho 1", "238;Josemberg;Cicleide;Vera Lúcia", "395;Jose Carlos;Edjane margarida;Sublime 1", "169;Justino;Miriam;Vera Lúcia ", "0;helder;Luciene ;Coquinho 1", "0;jose;eliane;Coquinho 1", "188;José Reginaldo;Damares Maria;Vera Lúcia", "452;Saulo;Priscila;Jardim Cajueiro 1", "198;Alexandro;Erli;Vera Lúcia", "313;Dc Célio;Maria José;Poço de Jacó", "327;Yasmim Ruana;;Poço de Jacó", "320;Jair;Jemima;Poço de Jacó", "316;Crislayne Eduarda;;Poço de Jacó", "312;Cosmo;Eduarda;Poço de Jacó", "5;Jeilson;Maria;Coquinho 1", "0;PB romero;neide;Veneza 2", "0;PB Euclides ;Monica ;Poço de Jacó", "0;cosmo;Eduarda ;Poço de Jacó", "0;pb Edvaldo ;erotildes;Vera Lúcia", "0;PB arão José ;aurizete ;Veneza 1", "0;PB Levi correia ; nely gomes;Jardim Cajueiro 1"];
+// Chama a função para carregar os nomes antes do sorteio
+carregarPresencas();
 
 //CÓDIGO DE SORTEIO
 var esperar = false; //Aguarda o sorteio para quando apertar TAB ele não fique chamando a função
